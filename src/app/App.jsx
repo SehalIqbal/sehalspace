3090b8c91c288c59d62f312913e1354b8b88eb06
Sehal Iqbal <sehaliqbal27@gmail.com>
2025-08-09 16:18:19 +0500

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { HeaderTop } from "../components";
+import { Post } from "../components";
+import Home from "../pages/home";
+import Elements from "../pages/themeElement";
+import Services from "../components/sections/services";
+import Skills from "../pages/about";
+import Contactme from "../pages/contactme";
+import Contact from "../pages/contact";
+import { useLocation, BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Blog from "../pages/blog";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <div className="rs-content">
+        <HeaderTop logotext="UBAI MUTL" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/elements" element={<Elements />} />
+          <Route path="/about" element={<Skills />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contactme" element={<Contactme />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
